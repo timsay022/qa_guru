a7588b8e6193f882aa6210672ad74debbf7d2339
timsay022 <saifitdinov.t@yandex.ru>
2024-11-22 00:18:18 +0500

    hw 1 - Done

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -12,5 +12,6 @@ public class SearchTests {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+
     }
 }
